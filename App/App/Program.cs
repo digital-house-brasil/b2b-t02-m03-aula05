17cf375e1ff6e223bf074570cc0d1d5b20dd5aba
Diego Pereira <dhiego_ti@hotmail.com>
2022-12-07 21:29:48 -0300

    exercicios

---
+++ App/App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            double media = 5;
+
+            //string resultado = (media >= 7) ? "Aprovado" : "Reprovado";
+            //Console.WriteLine(resultado);
+
+            //if (media >= 7)
+            //    Console.WriteLine("Aprovado");
+            //else if (media >= 5.1 && media < 7)
+            //    Console.WriteLine("Recuperação");
+            //else
+            //    Console.WriteLine("Reprovado");
+           
+            switch (media) {
+                case 8:
+                    Console.WriteLine("Aprovado");
+                    break;
+                case 5:
+                    Console.WriteLine("Reprovado");
+                    break;
+                default: 
+                    Console.WriteLine("Não indentificado");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
